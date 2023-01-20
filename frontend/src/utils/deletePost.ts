1096542086d0b856122271947c28ecca66cf97e0
Maciej Topor <maciej.topor@10clouds.com>
2023-01-20 12:57:38 +0100

    feat: added dryRun

--- frontend/src/utils/deletePost.ts
+++ frontend/src/utils/deletePost.ts
@@ -1,8 +1,6 @@
 import { ApiPromise } from '@polkadot/api';
 import { ContractPromise } from '@polkadot/api-contract';
 import { web3FromSource } from '@polkadot/extension-dapp';
-import type { WeightV2 } from '@polkadot/types/interfaces';
-import { BN } from '@polkadot/util';
 
 import { displayErrorToast, displaySuccessToast } from 'components/NotificationToast';
 
@@ -11,20 +9,13 @@ import { ErrorToastMessages } from 'shared/constants';
 
 import bulletinBoardMetadata from '../metadata/metadata_bulletin_board.json';
 import addresses from '../metadata/addresses.json';
-
-const REF_TIME = 14036466919;
-const PROOF_SIZE = 284124;
+import { getGasLimit } from './dryRun';
 
 export const deletePost = async (
   api: ApiPromise,
   loggedUser: InjectedAccountWithMeta,
   onSuccess: () => void
 ): Promise<void> => {
-  const gasLimit = api.registry.createType('WeightV2', {
-    refTime: new BN(REF_TIME).muln(2),
-    proofSize: new BN(PROOF_SIZE).muln(2),
-  }) as WeightV2;
-
   if (!loggedUser.meta.source) return;
 
   const contract = new ContractPromise(
@@ -33,6 +24,8 @@ export const deletePost = async (
     addresses.bulletin_board_address
   );
   const injector = await web3FromSource(loggedUser.meta.source);
+
+  const gasLimit = await getGasLimit(api, loggedUser.address, 'delete', contract);
 
   await contract.tx
     .delete({
